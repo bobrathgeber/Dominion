49d0f214f83961353ccb44fe6cf93567124827da
Bob <bob.rathgeber@gmail.com>
2011-10-16 03:51:12 -0400

    add card names enum

---
+++ Src/Dominion/CardNames.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dominion
+{
+    public enum CardNames
+    {
+        cellar,
+        chapel,
+        moat,
+        chancellor,
+        village,
+        woodcutter,
+        wordshop,
+        bureaucrat,
+        feast,
+        gardens,
+        militia,
+        moneylender,
+        remodel,
+        smithy,
+        spy,
+        thief,
+        throneRoom,
+        councilRoom,
+        festival,
+        laboratory,
+        library,
+        market,
+        mine,
+        witch,
+        adventurer,
+
+        curse,
+        estate,
+        duchy,
+        province,
+
+        copper,
+        silver,
+        gold,
+
+    }
+}
